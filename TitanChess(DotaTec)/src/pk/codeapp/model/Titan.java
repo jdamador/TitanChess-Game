fbe7443cabde502717ef595f1f539fdfa82b2fa1
jdamador <amadorsalasjd@gmail.com>
2017-09-24 23:20:32 -0600

    Show information from the titan

--- TitanChess(DotaTec)/src/pk/codeapp/model/Titan.java
+++ TitanChess(DotaTec)/src/pk/codeapp/model/Titan.java
@@ -1,6 +1,8 @@
 package pk.codeapp.model;
 import java.io.Serializable;
 import java.util.ArrayList;
+import javax.swing.DefaultListModel;
+import javax.swing.ListModel;
 import pk.codeapp.model.GraphicsElement;
 
 /*
@@ -19,16 +21,21 @@ public class Titan extends GraphicsElement implements Serializable{
     private int moves;
     private String vitalElement;
     private String icon;
+    private int damage;
+    
     //Relation
     private ArrayList<Attack> attacks = new ArrayList(); // List of Attacks
-    
-    public Titan(String name,int life,int moves,String vElement,String icon){
-        this.name=name;
-        this.life=life;
-        this.moves=moves;
-        this.vitalElement=vElement;
-        this.icon=icon;
+
+    public Titan(String name, int life, int moves, String vitalElement, String icon, int damage) {
+        this.name = name;
+        this.life = life;
+        this.moves = moves;
+        this.vitalElement = vitalElement;
+        this.icon = icon;
+        this.damage = damage;
     }
+    
+
     public String getName() {
         return name;
     }
@@ -55,6 +62,16 @@ public class Titan extends GraphicsElement implements Serializable{
 
     public String getVitalElement() {
         return vitalElement;
+    }
+
+    public DefaultListModel<String> toModel() {
+        DefaultListModel<String> model= new DefaultListModel();
+        model.addElement("Name:   "+this.name+ "    Vital Element:    "+this.vitalElement);
+        model.addElement("Life:   "+this.life + "     Maximun Damage  "+this.damage+"     Quantity Moves:  "+this.moves);
+       model.addElement("Attacks: ");
+       model.addElement(this.attacks+"");
+        
+        return model;
     }
     
 }
