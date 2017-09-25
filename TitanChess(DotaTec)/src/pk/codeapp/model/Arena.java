d47623f34d8106b1c0422b8831369db0698640d0
jdamador <amadorsalasjd@gmail.com>
2017-09-24 19:14:56 -0600

    Update packages

---
+++ TitanChess(DotaTec)/src/pk/codeapp/model/Arena.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jose Pablo Brenes
+ */
+public class Arena {
+    private String Elemento;
+    private int quantityColumns;
+    private int quantityRows;
+    private GraphicsElement[][] board;
+    public Arena(String Elemento,int qColums, int qRows){
+        this.Elemento = Elemento;
+        this.quantityColumns=qColums;
+        this.quantityRows=qRows;
+    }
+    public Arena(){}
+    public String getElemento() {
+        return Elemento;
+    }
+    public void setElemento(String Elemento) {
+        this.Elemento = Elemento;
+    }
+
+    public int getQuantityColumns() {
+        return quantityColumns;
+    }
+
+    public void setQuantityColumns(int quantityColumns) {
+        this.quantityColumns = quantityColumns;
+    }
+
+    public int getQuantityRows() {
+        return quantityRows;
+    }
+
+    public void setQuantityRows(int quantityRows) {
+        this.quantityRows = quantityRows;
+    }
+    
+}
