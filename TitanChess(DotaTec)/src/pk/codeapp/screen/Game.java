1ed949aec6c9f297bfc78087d1e612c1b1f58f6c
jdamador <amadorsalasjd@gmail.com>
2017-09-30 19:27:03 -0600

    Revert "add more estadistics"
    This reverts commit f9c1afbc22f84cb4ad6f556b74e1b458099d6f4b.

--- TitanChess(DotaTec)/src/pk/codeapp/screen/Game.java
+++ TitanChess(DotaTec)/src/pk/codeapp/screen/Game.java
@@ -823,7 +823,7 @@ public class Game extends javax.swing.JFrame implements DefaultRules, ActionList
                 dupla = ((Tower) element).getPosition();
                 graphicsElements[dupla.getColumn()][dupla.getRow()] = null;
                 Path path = gameSettings.searchButtonToPaint(buttons, dupla.getColumn(), dupla.getRow());
-                increaseDestroyTowers(tower);
+                increaseDetroyTowers(tower);
                 path.setIcon(new ImageIcon("src/pk/codeapp/tools/deletetower.png"));
                 path.setEnabled(false);
             }
@@ -864,21 +864,17 @@ public class Game extends javax.swing.JFrame implements DefaultRules, ActionList
             updateEstadics(estadisticsPlayer2, index);
         }
     }
-    private void increaseDestroyTowers(Tower tower)
+    private void increaseDetroyTowers(Tower tower)
     {
         int index ;
         if (tower.getTowerPlayer().equalsIgnoreCase("Player1")) {
            index = searchUserToEdit(methods.getActual());
             estadisticsPlayer1.setDrestroyTower(estadisticsPlayer1.getDrestroyTower()+ 1);
             updateEstadics(estadisticsPlayer1, index);
-            estadisticsPlayer2.setMyDeadTower(estadisticsPlayer2.getMyDeadTower()+1);
-            updateEstadics(estadisticsPlayer2, index);
         } else {
             index = searchUserToEdit(methods.getPlayer2());
             estadisticsPlayer2.setDrestroyTower(estadisticsPlayer1.getDrestroyTower() + 1);
             updateEstadics(estadisticsPlayer2, index);
-             estadisticsPlayer1.setMyDeadTower(estadisticsPlayer1.getMyDeadTower()+ 1);
-            updateEstadics(estadisticsPlayer1, index);
         }
     }
    public void updateEstadics(Estadistics estadistics, int index){
